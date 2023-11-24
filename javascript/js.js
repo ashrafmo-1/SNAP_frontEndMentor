458f2dbbc78ae45bd6b8d990314ca571f2c0b681
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-11-24 20:15:24 +0200

    edit mobile screen

--- javascript/js.js
+++ javascript/js.js
@@ -3,6 +3,7 @@ let companyLinks = document.querySelector('.moreCompany');
 // media screen 
 let xMediam = window.matchMedia("(max-width: 1022px)");
 let xSmall = window.matchMedia("(max-width: 860px)");
+let xMobile = window.matchMedia("(max-width: 650px)"); // mobile
 
 function featuersToggleActive() {
     featuersLinks.classList.toggle('active');
@@ -24,8 +25,8 @@ function mediaMediam(x) {
         document.querySelector('.container').style.width = '';
     }
 };
-mediaMediam(xMediam)
-xMediam.addListener(mediaMediam)
+mediaMediam(xMediam);
+xMediam.addListener(mediaMediam);
 
 function mediaSmall(X) {
     if(X.matches) {
@@ -40,8 +41,37 @@ function mediaSmall(X) {
         document.querySelector('.landing .textcontent .title').style.fontSize = '';
         document.querySelectorAll('.allCatigorys .img').forEach((ele) => {
             ele.style.width = ''
-        })
+        });
+    };
+};
+mediaSmall(xSmall);
+xSmall.addListener(mediaSmall);
+
+
+function mediaMobile(X) {
+    if(X.matches) {
+        // edit image style lication
+        document.querySelector('.landingImage').style.display = "none";
+        document.querySelector('.textcontent').style.display = "flex";
+        document.querySelector('.textcontent').style.justifyContent = 'center'
+        document.querySelector('.textcontent').style.alignItems = 'center'
+        document.querySelector('.textcontent').style.flexDirection = 'column'
+        document.querySelector('.textcontent').style.height = '100vh'
+
+        // image top
+        document.querySelector('.landingImageMobile').style.display = 'block';
+        document.querySelector('.landingImageMobile').style.width = '100%';
+        document.querySelector('.landingImageMobile').style.marginTop = '50px';
+    }else{
+        document.querySelector('.landingImage').style.display = "block";
+        document.querySelector('.textcontent').style.display = "";
+        document.querySelector('.textcontent').style.justifyContent = ''
+        document.querySelector('.textcontent').style.alignItems = ''
+        document.querySelector('.textcontent').style.flexDirection = ''
+        document.querySelector('.textcontent').style.height = ''
+        // image top
+        document.querySelector('.landingImageMobile').style.display = '';
     }
 }
-mediaSmall(xSmall)
-xSmall.addListener(mediaSmall)
+mediaMobile(xMobile);
+xMobile.addListener(mediaMobile);
