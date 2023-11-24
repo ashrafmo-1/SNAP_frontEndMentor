851dbadbf2d567d87ac5701a30a96a74c85f09f5
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-11-24 07:22:26 +0200

    first deployment

---
+++ javascript/js.js
@@ -0,0 +1,12 @@
+let featuersLinks = document.querySelector('.moreFeatuers');
+let companyLinks = document.querySelector('.moreCompany');
+
+function featuersToggleActive() {
+    featuersLinks.classList.toggle('active');
+}
+
+function companyToggleActive() {
+    companyLinks.classList.toggle('active');
+}
+
+console.log(featuersLinks);
